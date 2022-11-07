d7abebc522b1392a4a238f13ad26eb1bdf90f35f
ib-bib <81387641+ib-bib@users.noreply.github.com>
2022-11-07 07:40:17 +0000

    Add files via upload

---
+++ stack.ts
@@ -0,0 +1,35 @@
+type item = number | string;
+
+let stackTop: number = -1;
+const stackCap = 1090519040;
+const stackArr: item[] = [];
+
+const stackPush = (x: item) => {
+    if (isFull()) {
+        console.log("Cannot insert more items!");
+        return;
+    }
+    stackArr[++stackTop] = x;
+}
+
+const stackPop = () => {
+    if (isEmpty()) {
+        return "Nothing to pop. Stack is empty.";
+    }
+    return stackArr[stackTop--];
+}
+
+const stackPeek = () => {
+    if (isEmpty()) {
+        return "Nothing to peek. Stack is empty.";
+    }
+    return stackArr[stackTop];
+}
+
+const isEmpty = () => {
+    return stackTop === -1;
+}
+
+const isFull = () => {
+    return stackTop === stackCap;
+}
